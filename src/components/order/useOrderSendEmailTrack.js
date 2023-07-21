cf6a26ebd0591ffe74809fca179726c8f6808475
enotowitch <en.enotowitch@gmail.com>
2023-07-21 09:35:24 +0300

    M

--- src/components/order/useOrderSendEmailTrack.js
+++ src/components/order/useOrderSendEmailTrack.js
@@ -4,9 +4,10 @@ import parseForm from "../../utils/parseForm"
 export default function useOrderSendEmailTrack() {
 
 	async function sendEmail(e) {
+		e.preventDefault()
 		const { form } = parseForm(e)
 		const res = await api.orderSendEmailTrack(form)
-		console.log(res)
+		res.ok && (window.location.href = "/") // TODO go to "/orders" and remove `order` to which email admin sent track
 	}
 
 	return (
