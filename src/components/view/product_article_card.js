94eeb654aae9d6bf48fee2d17d5212d40b1b886b
enotowitch <en.enotowitch@gmail.com>
2023-07-25 13:35:10 +0300

    +card FIX

--- src/components/view/product_article_card.js
+++ src/components/view/product_article_card.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import liked from "../../img/liked.svg"
 import { Clear, Edit } from "@mui/icons-material";
 import Tags from '../tags/Tags';
@@ -7,23 +7,22 @@ export default function Product_article_card({ obj, children }) {
 
 	// !! only props must be here, no hooks
 	// * gray cause of eval
-	const { img, title, price, type, _id, deletePost, tags } = obj
-	const [show, showSet] = useState(false) // TODO ??? separate hook
+	const { img, title, type, _id, deletePost, tags } = obj
 
 	return (
-		<div onMouseEnter={() => showSet(true)} onMouseLeave={() => showSet(false)}>
-			{show &&
-				<div className="f jcsb aic card__top" onClick={(e) => e.stopPropagation()}>
-					<img src={liked} className="ml010" />
-					{/* eg: /edit/product|article|smth/_id */}
-					<Edit style={{ height: 33 }} onClick={() => window.location.href = `/edit/${type}/${_id}`} />
-					<Clear onClick={(e) => deletePost(e, type, _id)} className="mr010" />
-				</div>
-			}
+		<div>
+			{/* top */}
+			<div className="f jcsb aic card__top" onClick={(e) => e.stopPropagation()}>
+				<img src={liked} className="ml010" />
+				{/* eg: /edit/product|article|smth/_id */}
+				<Edit style={{ height: 33 }} onClick={() => window.location.href = `/edit/${type}/${_id}`} />
+				<Clear onClick={(e) => deletePost(e, type, _id)} className="mr010" />
+			</div>
+			{/* center */}
 			<img src={img} />
 			<Tags arr={tags} className="scrollableTags" />
 			<div className="title tac">{title}</div>
-
+			{/* bottom */}
 			{children}
 		</div>
 	)
