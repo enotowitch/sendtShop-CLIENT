3d28d941d411cb3e74f2f4bd30650dd6cea70ba9
enotowitch <en.enotowitch@gmail.com>
2023-07-25 14:19:33 +0300

    +Draggable Tags on DeskTop

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,18 +1,21 @@
 import React from "react"
 import Tag from "./Tag"
 import "./index.scss"
+import Draggable from "../other/Draggable"
 
 export default function Tags({ arr, className }) {
 
 	typeof arr === 'string' && (arr = arr.split(','))
 
 	return (
-		<div className={className} onClick={(e) => e.stopPropagation()}>
-			{arr?.map((tag, ind) => {
-				return (
-					<Tag key={ind} tag={tag} />
-				)
-			})}
-		</div>
+		<Draggable>
+			<div className={className} onClick={(e) => e.stopPropagation()}>
+				{arr?.map((tag, ind) => {
+					return (
+						<Tag key={ind} tag={tag} />
+					)
+				})}
+			</div>
+		</Draggable>
 	)
 }
