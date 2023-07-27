1a153c72f1a946e3cebdbc65629f88521a9eff74
enotowitch <en.enotowitch@gmail.com>
2023-07-27 18:33:42 +0300

    +product_full brand

--- src/components/view/product_full.js
+++ src/components/view/product_full.js
@@ -5,7 +5,7 @@ import { CART_ROUTE } from '../../consts'
 export default function product_full({ obj }) {
 
 	// !! only props must be here, no hooks
-	const { img, title, price, text, _id: productId } = obj.fullPost
+	const { img, title, price, text, _id: productId, brand } = obj.fullPost
 	const { pullPush } = obj
 
 	return (
@@ -16,15 +16,18 @@ export default function product_full({ obj }) {
 			<div className="postFull__right">
 				<div className="title tac">{title}</div>
 				<div className="brand tac">${price}</div>
-				<hr></hr>
-				<div className="tac">{text}</div>
+				<div className="gray tac mt2">{brand}</div>
+				<div className="tac">stars here</div>
+				<div className="gray tac">123 customer reviews</div>
 				<Button
-					className="mt"
+					className="mt2"
 					onClick={() => (window.location.href = CART_ROUTE, pullPush({ col: "user", field: "cart", item: productId, action: "push", dups: true }))}
 					variant="contained"
 				>
 					add to cart
 				</Button>
+				<hr className="my2"></hr>
+				<div className="tac">{text}</div>
 			</div>
 		</>
 	)
