24db95dd0d8826b488477a9e5cee33d10d543343
enotowitch <en.enotowitch@gmail.com>
2023-07-27 17:13:57 +0300

    +close mobile menu on header icon click

--- src/components/header/HeaderIcon.js
+++ src/components/header/HeaderIcon.js
@@ -1,16 +1,18 @@
-import React, { cloneElement } from "react"
+import React, { cloneElement, useContext } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { BRAND_COLOR } from "../../consts"
+import { Context } from "../../Context"
 
 export default function HeaderIcon({ children, route }) {
 
 	const { pathname } = useLocation()
+	const { showMenuSet } = useContext(Context) // close mobile menu on header icon click
 
 	return (
 		pathname === route
 			?
-			<Link to={route}>{cloneElement(children, { style: { fill: BRAND_COLOR } })}</Link>
+			<Link to={route} onClick={() => showMenuSet(false)}>{cloneElement(children, { style: { fill: BRAND_COLOR } })}</Link>
 			:
-			<Link to={route}>{children}</Link>
+			<Link to={route} onClick={() => showMenuSet(false)}>{children}</Link>
 	)
 }
