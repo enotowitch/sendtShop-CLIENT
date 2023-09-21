a6dfd08f044967de743e5bd815b601c4520b6529
enotowitch <en.enotowitch@gmail.com>
2023-09-21 10:13:09 +0300

    123

--- src/components/view/product_full.js
+++ src/components/view/product_full.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Product_article_card_top from "./product_article_card_top"
 import Tags from "../tags/Tags"
 import Product_full_form from "./Product_full_form"
@@ -10,38 +10,30 @@ import Product_full_tabs from "./Product_full_tabs"
 
 export default function Product_full({ obj }) {
 
-	let img, title, price, text, productId, brand, tags, archive, characteristics, informations
+	let img, title, price, text, productId, brand, tags, archive, characteristicNames, characteristicValues, informationNames, informationValues
 	if (obj.fullPost) { // prevent "can not destructure"
-		({ img, title, price, text, _id: productId, brand, tags, archive, characteristics, informations } = obj.fullPost)
+		({ img, title, price, text, _id: productId, brand, tags, archive, characteristicNames, characteristicValues, informationNames, informationValues } = obj.fullPost)
 	}
 
-	const { addToCart, hidePost } = obj
-
-	// ! change price on productId change
-	const [finalVarPrice, finalVarPriceSet] = useState(0)
-	useEffect(() => {
-		finalVarPriceSet(price)
-	}, [productId])
-	// ? change price on productId change
-
+	const { addToCart, deletePost } = obj
+	const [varPrice, varPriceSet] = useState(price)
 
 	return (
 		<>
 			{/* left */}
 			<div className="postFull__left">
-				<Product_article_card_top obj={{ ...obj.fullPost, hidePost }} />
+				<Product_article_card_top obj={{ ...obj.fullPost, deletePost }} />
 				{img && <CarouselProduct arr={img} />}
-				{characteristics && <Product_full_tabs characteristics={characteristics} informations={informations} productId={productId} />}
+				{characteristicNames && <Product_full_tabs obj={{ characteristicNames, characteristicValues, informationNames, informationValues }} productId={productId} />}
 			</div>
 			{/* right */}
 			<div className="postFull__right">
 				<div className="title tac">{title}</div>
-				{/* finalVarPrice: 0: additional price was NOT applied || initial price */}
-				<div className="brand tac fullProductTotalPrice">${finalVarPrice || price}</div>
+				<div className="brand tac fullProductTotalPrice">${varPrice}</div>
 				<div className="gray tac mt mb">{brand}</div>
 				<RatingCount _id={productId} showCount={true} />
 				<HasDigitalDownload download={archive} className="fcc" />
-				<Product_full_form addToCart={addToCart} {...obj.fullPost} finalVarPriceSet={finalVarPriceSet} />
+				<Product_full_form addToCart={addToCart} {...obj.fullPost} />
 				{/* "bottom" */}
 				<hr className="mt2 mb"></hr>
 				<Tags arr={tags} className="tac" label="Tags" />
@@ -50,4 +42,3 @@ export default function Product_full({ obj }) {
 		</>
 	)
 }
-// test com
